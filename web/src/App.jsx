292de49962357654cfa77d0875ce4ab078b25f11
GILBERT SEBASTIAN YOGIANA <z5641572@unsw.edu.au>
2026-09-07 12:58:30 +0700

    fixes

--- web/src/App.jsx
+++ web/src/App.jsx
@@ -1,7 +1,4 @@
 import { useEffect, useState } from 'react'
-import heroImg from './assets/hero.png'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
 import './App.css'
 import { getEntry, saveEntry } from './api/entries'
 
